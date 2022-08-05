d01c7ad7213ab8c45b87f949b8db5b67b26abb4d
Marc Solà Nadal <marcsolanadal@gmail.com>
2022-08-05 19:31:11 +0200

    Refactor histogram with functions, structs and pointers

--- 1-a-tutorial-introduction/ex1-13-word-histogram.c
+++ 1-a-tutorial-introduction/ex1-13-word-histogram.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
 
-void Histogram_printBar(int length);
-void Histogram_print(int numbers[], int whitespace_count, int other_count);
-
 typedef struct Histogram {
   int digit_count[10];
   int whitespace_count;
   int other_count;
 } Histogram;
 
+void Histogram_printBar(int length);
+void Histogram_print(Histogram *hist);
+void Histogram_count(Histogram *hist);
+
 int main() {
-  int c, word_count, whitespace_count, other_count;
-  int digit_count_list[10];
+  Histogram hist = { 0 };
 
-  word_count = whitespace_count = other_count = 0;
-
-  // Count ocurrences
-  while((c = getchar()) != EOF) {
-    if(c == ' ') {
-      whitespace_count += 1;
-    } else if(c >= '0' && c <= '9') {
-      ++digit_count_list[c-'0'];
-    } else {
-      other_count += 1;
-    }
-  }
-
-  Histogram_print(digit_count_list, whitespace_count, other_count);
+  Histogram_count(&hist);
+  Histogram_print(&hist);
   
   return 0;
+}
+
+void Histogram_count(Histogram *hist) {
+  int c;
+  while((c = getchar()) != EOF) {
+    if(c == ' ') {
+      hist->whitespace_count += 1;
+    } else if(c >= '0' && c <= '9') {
+      hist->digit_count[c-'0'] += 1;
+    } else {
+      hist->other_count += 1;
+    }
+  }
 }
 
 void Histogram_printBar(int length) {
@@ -39,19 +40,19 @@ void Histogram_printBar(int length) {
     printf(">\n");
 }
 
-void Histogram_print(int numbers[], int whitespace_count, int other_count) {
+void Histogram_print(Histogram *hist) {
   printf("--------------------------------------------\n");
   printf("HISTOGRAM\n");
   printf("--------------------------------------------\n\n");
 
   for(int i = 0; i < 10; i++) {
     printf("%10d", i);
-    Histogram_printBar(numbers[i]);
+    Histogram_printBar(hist->digit_count[i]);
   }
  
   printf("%10s", "Space");
-  Histogram_printBar(whitespace_count);
+  Histogram_printBar(hist->whitespace_count);
 
   printf("%10s", "Other");
-  Histogram_printBar(other_count);
+  Histogram_printBar(hist->other_count);
 }
